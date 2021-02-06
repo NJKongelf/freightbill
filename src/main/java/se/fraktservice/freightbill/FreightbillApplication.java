39f816fdb7897663751e3d23fdfacf08302fb38b
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2021-02-06 18:54:48 +0100

    Initial commit

---
+++ src/main/java/se/fraktservice/freightbill/FreightbillApplication.java
@@ -0,0 +1,13 @@
+package se.fraktservice.freightbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightbillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreightbillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/fraktservice/freightbill/FreightbillApplicationTests.java
@@ -0,0 +1,13 @@
+package se.fraktservice.freightbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightbillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
